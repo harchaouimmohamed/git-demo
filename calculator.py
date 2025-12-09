5b91355a987a6a7a597f1fd753cd43e109b22883
MalvX0r <147427922+MalvXor@users.noreply.github.com>
2025-12-09 20:36:20 +0100

    Add power function to calculator

--- calculator.py
+++ calculator.py
@@ -5,6 +5,7 @@ def add(x, y):
 def subtract(x, y):
     """Function to subtract two numbers"""
     return x - y
+
 def division(x, y):
     """Function to divide two numbers"""
     if y == 0:
@@ -12,5 +13,9 @@ def division(x, y):
     return x / y
 
 def multiply(x, y):
-    """Function to subtract two numbers"""
+    """Function to multiply two numbers"""
     return x * y
+
+def power(x, y):
+    """Function to raise x to the power of y"""
+    return x ** y
